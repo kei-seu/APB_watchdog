ca5918fecaa55c6fde8abcd6c85279af074aec03
kei-seu <904812013@qq.com>
2023-08-03 17:06:56 +0800

    fix UVM verification platform-env-kei_watchdog_pkg.sv

--- kei_cmsdk_apb_watchdog_tb/env/kei_watchdog_pkg.sv
+++ kei_cmsdk_apb_watchdog_tb/env/kei_watchdog_pkg.sv
@@ -14,8 +14,8 @@ package kei_watchdog_pkg;
             
   `include "kei_watchdog_scoreboard.sv"
   `include "kei_watchdog_virtual_sequencer.sv"
-  `include "kei_watchdog_env.sv"
   `include "kei_watchdog_adapter.sv"
+  `include "kei_watchdog_env.sv"
             
   `include "kei_watchdog_seq_lib.svh"
   `include "kei_watchdog_tests.svh"
