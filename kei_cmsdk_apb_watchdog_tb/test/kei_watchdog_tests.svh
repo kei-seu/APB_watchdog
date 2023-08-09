fcb7d2b5341583ff96083152062f53ce073e4d68
kei-seu <904812013@qq.com>
2023-08-09 22:22:40 +0800

    update UVM verification platform-test-kei_watchdog_tests.svh

--- kei_cmsdk_apb_watchdog_tb/test/kei_watchdog_tests.svh
+++ kei_cmsdk_apb_watchdog_tb/test/kei_watchdog_tests.svh
@@ -4,5 +4,6 @@
 `include "kei_watchdog_base_test.sv"
 `include "kei_watchdog_apbacc_test.sv"
 `include "kei_watchdog_regacc_test.sv"
+`include "kei_watchdog_integration_test.sv"
 
 `endif // KEI_WATCHDOG_TESTS_SVH
