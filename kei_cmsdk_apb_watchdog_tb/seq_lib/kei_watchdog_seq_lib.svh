be735e5949586d08a2490f88bb17937f726b0890
kei-seu <904812013@qq.com>
2023-08-13 20:20:49 +0800

    update UVM verification platform-seq_lib-kei_watchdog_seq_lib.svh

--- kei_cmsdk_apb_watchdog_tb/seq_lib/kei_watchdog_seq_lib.svh
+++ kei_cmsdk_apb_watchdog_tb/seq_lib/kei_watchdog_seq_lib.svh
@@ -6,5 +6,6 @@
 `include "kei_watchdog_apbacc_virt_seq.sv"
 `include "kei_watchdog_regacc_virt_seq.sv"
 `include "kei_watchdog_integration_virt_seq.sv"
+`include "kei_watchdog_resen_virt_seq.sv"
 
 `endif 
